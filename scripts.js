a0f45a6c9aaea5aee1d5cceb68a1b533112df5a8
aldrichsaladino <aldrich.saladino@gmail.com>
2024-12-01 15:30:19 -0700

    Created nessary files and added some baseline html code

---
+++ scripts.js
@@ -0,0 +1,11 @@
+/* This script is designed to emulate the functionality and operations of a basic calculator, similar to one you can find on your phone. Functionality consists of the following: 1) Core operations such as +, -, /, * 2) Additions such as Clear, Backspace and percentages, 3) a hyphen to value swap */
+
+// Link script to Index and declare variables based on buttons in HTML via onclick.
+
+
+// Based on the clickable buttons, create functions to perform the actions
+
+// Add ability to use both clickable buttons or keyboard entry - Need to figure out how
+
+// List all operations and functions
+
